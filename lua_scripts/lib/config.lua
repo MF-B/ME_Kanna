36f703eadffebd69d7bf1cecbc7b3be663b81f32
mf1bzz-desktop <guzemin@hotmail.com>
2026-02-15 18:00:26 +0800

    test1

--- lua_scripts/lib/config.lua
+++ lua_scripts/lib/config.lua
@@ -1,8 +1,8 @@
 local M = {}
 
 -- ================= 配置区域 =================
-M.HOST = "http://192.168.1.34:8080"
-M.WS_URL = "ws://192.168.1.34:8080/ws/minecraft"
+M.HOST = "http://192.168.1.20:8080"
+M.WS_URL = "ws://192.168.1.20:8080/ws/minecraft"
 M.API_URL = M.HOST .. "/config/whitelist"
 M.DEVICE_ID = "ae_hub"
 
